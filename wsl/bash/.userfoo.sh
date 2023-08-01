c3b3ba7a12818d06ac9fc3be90ee06bb318a2bb9
Arne Hetzenegger <arne.hetzenegger@adesso.de>
2023-08-01 16:37:35 +0200

    Fix modifyBashrc in wsl/bash/.userfoo.sh for search string containing spaces

--- wsl/bash/.userfoo.sh
+++ wsl/bash/.userfoo.sh
@@ -8,8 +8,8 @@ HOMEDIR=""
 modifyBashrc () {
   searchFor=${1}
   writeString=${2}
-  if [[ $(cat ${HOMEDIR}/.bashrc | grep ${searchFor} | wc -l) == 0 ]]; then
-    echo ${writeString} >> ${HOMEDIR}/.bashrc
+  if [[ $(cat ${HOMEDIR}/.bashrc | grep "${searchFor}" | wc -l) == 0 ]]; then
+    echo "${writeString}" >> ${HOMEDIR}/.bashrc
     . ${HOMEDIR}/.bashrc
   else
     echo "${searchFor}: ${HOMEDIR}/.bashrc is already properly configured."
